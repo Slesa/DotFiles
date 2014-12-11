0e2b95caec1241291b23a2fa351c784eed732e7b
Jörg Preiß <joerg.preiss@slesa.de>
2014-12-11 17:03:14 +0100

    Appending git tool shell scripts, bash aliases

---
+++ bin/tools/git/meld-diff.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#
+# export GIT_DIFFMERGE_VERBOSE=1 to enable logging
+#
+
+script_path=${0%/*}
+source "$script_path/diff-and-merge-support.sh"
+
+get_cygpath path
+old=$($path --mixed "$1")
+new=$($path --mixed "$2")
+
+difftool="/c/Program Files (x86)/Meld/meld.exe"
+
+"$difftool" --diff "$old" "$new"
