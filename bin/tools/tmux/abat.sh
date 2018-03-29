ba7413ca917abbb7fee276334a86c949b2a17094
Jörg Preiß <joerg.preiss@slesa.de>
2018-03-29 22:38:11 +0200

    Allow tmux on cygwin. Add paths

--- bin/tools/tmux/abat.sh
+++ bin/tools/tmux/abat.sh
@@ -1,15 +1,20 @@
-tmux has-session -t abat
+tmux -S ~/.tmux has-session -t abat
 if [ $? != 0 ]
 then
-	tmux new-session -s abat -n cpp -d
-	tmux send-keys -t abat 'cd ~abat/Plus_Cpp' C-m
+	tmux -S ~/.tmux new-session -s abat -n cpp -d
+	tmux -S ~/.tmux send-keys -t abat 'cd ~abat/Plus_Cpp' C-m
 
-	tmux new-window -n client -t abat
-	tmux send-keys -t abat:2 'cd ~abat/Plus_Client' C-m
+	tmux -S ~/.tmux new-window -n client -t abat
+	tmux -S ~/.tmux send-keys -t abat:2 'cd ~abat/Plus_Client' C-m
 
-	tmux split-window -v -t abat:1
-	tmux split-window -v -t abat:2
+	tmux -S ~/.tmux new-window -n tools -t abat
+	tmux -S ~/.tmux send-keys -t abat:3 'cd ~abat/Plus_Tools' C-m
 
-	tmux select-window -t abat:1
+
+	tmux -S ~/.tmux split-window -v -t abat:1
+	tmux -S ~/.tmux split-window -v -t abat:2
+	tmux -S ~/.tmux split-window -v -t abat:3
+
+	tmux -S ~/.tmux select-window -t abat:1
 fi
-tmux attach -t abat
+tmux -S ~/.tmux attach -t abat
